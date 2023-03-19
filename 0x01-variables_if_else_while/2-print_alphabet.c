9cc395649659cd479f56b327ebdba0118a086c24
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-19 10:51:22 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+char letter = 'a';
+while (letter <= 'z') {
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return 0;
+}
+
