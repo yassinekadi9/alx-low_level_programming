f3124a0eb35dbfc9bc893775af63f3370124ee1e
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-21 11:20:42 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,18 +5,17 @@
 * Description: 'prints _putchar to stdout'
 * Return: always 0
 */
-int main (void)
+int main(void)
 {
-
- _putchar("_");
- _putchar("p");
- _putchar("u");
- _putchar("t");
- _putchar("c");
- _putchar("h");
- _putchar("a"); 
- _putchar("r"); 
- _putchar("\n");
-
-return (0);
+	 _putchar("_");
+	 _putchar("p");
+	 _putchar("u");
+	 _putchar("t");
+	 _putchar("c");
+	 _putchar("h");
+	 _putchar("a"); 
+	 _putchar("r"); 
+	 _putchar("\n");
+	 
+	 return (0);
 }
