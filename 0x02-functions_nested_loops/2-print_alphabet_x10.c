f5b789a2ce577aa45229932c33b882360c59fe6d
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-21 18:31:04 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,12 @@ void print_alphabet_x10(void)
 {
 	int x, y;
 
-	for (y = 0; y < 10; y++)
-{
-	for (x = 'a'; x <= 'z'; x++)
-
-		_putchar(x);
-	_putchar('\n');
-
-	return (0);
-}
-
+	for (y = 0; y <= 9; y++)
+		{
+			for (x = 'a'; x <= 'z'; x++)
+				_putchar(x);
+			_putchar('\n');
+			return (0);
+		}
 }
 
