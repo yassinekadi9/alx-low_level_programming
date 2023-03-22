4076bd1f35e8156b8b0fcf1e446bf9b1afb0e310
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-21 17:06:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,11 @@
 */
 int main(void)
 {
-	 _putchar('_');
-	 _putchar('p');
-	 _putchar('u');
-	 _putchar('t');
-	 _putchar('c');
-	 _putchar('h');
-	 _putchar('a'); 
-	 _putchar('r'); 
-	 _putchar('\n');
-	 
-	 return (0);
+char str[] = "_putchar";
+int ch;
+
+for (ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
 }
