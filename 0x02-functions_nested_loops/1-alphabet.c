1939c8dc04b333dedd9243b8c45dad7a7bec82ef
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-21 18:02:09 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,10 @@
 * print_alphabet - utilizes on the _putchar function
 * the alphabet a - z
 */
-
 void print_alphabet(void)
 {
 	int ch;
-	
+
 	for (ch = 'a'; ch <= 'z' ; ch++)
 		_putchar(ch);
 	_putchar('\n');
