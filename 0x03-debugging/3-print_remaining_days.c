16b9c46a7cd263559e9c5d41deca54c02b97f260
yassinekadi9 <yassinekadi245@gmail.com>
2023-03-23 17:28:23 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * leap year when it can be devided
-	 * by 100 and 400 or by 4 evenly
-	*/
+/**
+ * leap year when it can be devide
+ * by 100 and 400 or by 4 evenly
+*/
 if ((year % 100 == 0 &&  year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
