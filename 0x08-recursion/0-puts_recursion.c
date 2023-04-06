65b96a951473ce96934f6d96866ecd70ad9a18b1
yassinekadi9 <yassinekadi245@gmail.com>
2023-04-06 08:59:59 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,17 @@
  */
 int main(void)
 {
-    void _puts_recursion(char *s);
-    {
-	    if(*s == '\0'){
-		    putchar('\n');
-                    return;
-	    }
-	    putchar(*s);
-	    _puts_recursion(s + 1);
+    #include <stdio.h>
+
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') {
+        putchar('\n');
+        return;
     }
-    return (0);
+
+    putchar(*s);
+    _puts_recursion(s + 1);
+}
+return(0);
 }
