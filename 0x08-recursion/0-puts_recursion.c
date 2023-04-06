09943bd7f2fafd4fc67eb2ef4676e2a998a4d726
yassinekadi9 <yassinekadi245@gmail.com>
2023-04-06 08:57:17 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    void _puts_recursion(char *s);
+    {
+	    if(*s == '\0'){
+		    putchar('\n');
+                    return;
+	    }
+	    putchar(*s);
+	    _puts_recursion(s + 1);
+    }
+    return (0);
+}
