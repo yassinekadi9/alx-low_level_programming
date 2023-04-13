5ad8b0b48cae3adbdddb7734fb789e206472f21a
yassinekadi9 <yassinekadi245@gmail.com>
2023-04-13 10:51:10 -0700

    more malloc free project

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,9 +3,9 @@
 
 /**
  * *_memset - fills memory with a constant byte
- * @s: memory area to be filled	
+ * @s: memory area to be filled
  * @b: char to copy
- * @n: number of times to copy b		
+ * @n: number of times to copy b
  *
  * Return: pointer to the memory area s
 */
